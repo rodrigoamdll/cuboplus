cae382fa05e3859cf95266a13202c301baa100d0
rodribp <rodrigobonilla177@gmail.com>
2023-12-13 12:47:14 -0600

    remade way to get language, localstorage implemented and single file for each language

--- src/script.js
+++ src/script.js
@@ -64,19 +64,40 @@ function setElementsNavbarAndFooter(content) {
   document.getElementById("rights").innerHTML = content.footer[3];
 }
 
-//function to set the 
+//function to set the language in page
 async function changeLanguage(page, language, callback) {
   //validates localStorage is not already settled before with a language non-declarated
   //language == "en" || "es"
   //if NOT language = "en"
-  if (language != "en" && language != "es") language = "en";
-
-  localStorage.setItem("language", language);
-  const layoutRes = await fetch(`./content/layout-${language}.json`)
-  const layout = await layoutRes.json();
-  const response = await fetch(`./content/${page}-${language}.json`);
-  const content = await response.json();
+  if (language !== "en" && language !== "es") language = "en";
   
-  setElementsNavbarAndFooter(layout);
-  callback(content);
+  //sets language defined
+  localStorage.setItem("language", language);
+
+  // Check if content exists in localStorage and has a valid 'secret' property
+  const storedContent = localStorage.getItem(`content-${language}`);
+
+  
+
+  try {
+    const content = JSON.parse(storedContent);
+    if (content && (content.secret == "es-cuboplus" || content.secret == "en-cuboplus")) {
+      // Content is valid, proceed with the callback
+      setElementsNavbarAndFooter(content.layout);
+      callback(content[page]);
+      return;
+    }
+  } catch (error) {
+    // Handle the error, e.g., clear localStorage and fetch content again
+    localStorage.removeItem(`content-${language}`);
+  }
+
+  // If content does not exist or does not have a valid 'secret', fetch and store it
+  const res = await fetch(`./content/content-${language}.json`);
+  const res1 = await res.json();
+
+  localStorage.setItem(`content-${language}`, JSON.stringify(res1));
+
+  setElementsNavbarAndFooter(res1.layout);
+  callback(res1[page]);
 }
